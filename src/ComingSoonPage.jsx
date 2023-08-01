e5357c9bbb34f5f4823922f276743a9c5d4acb51
Kartik Singh <kartiksingh389@gmail.com>
2023-08-01 11:44:09 +0530

    form

--- src/ComingSoonPage.jsx
+++ src/ComingSoonPage.jsx
@@ -10,7 +10,13 @@ const ComingSoonPage = () => {
         <div className="gridbg content ">
         <div className="box">
         <span className='company'>Sarvottam Megapolis</span>
-           <span className='coming'>Coming Soon..</span>
+           <span className='coming'>Coming Soon 
+  
+
+   <span  className="waviy span">.</span>
+   <span  class="waviy sec">.</span>
+
+</span>
           <span className='desc'>1600 Acre Green Hi-tech City <br/> Opposite Bodaki Railway Junction, Greater Noida,           Gautam Buddha Nagar, U.P</span>
           </div>
         </div>
