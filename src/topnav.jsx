5c01e655907f1866c6aed470995fde25e8a71c1c
Kartik Singh <kartiksingh389@gmail.com>
2025-01-03 17:59:09 +0530

    location and sector map changes

--- src/topnav.jsx
+++ src/topnav.jsx
@@ -4,9 +4,10 @@ import './topnavbar.css';
 const TopNavbar = () => {
   const [showOverlay, setShowOverlay] = useState(false);
   const [pdfUrl, setPdfUrl] = useState('');
+  const [showSectorButtons, setShowSectorButtons] = useState(false);
 
   const openOverlay = (pdf) => {
-    setPdfUrl(pdf);  // Set the PDF URL based on which button was clicked
+    setPdfUrl(pdf);
     setShowOverlay(true);
   };
 
@@ -18,10 +19,65 @@ const TopNavbar = () => {
     <div className="top-navbar">
       <button className="nav-button">Home</button>
       <button className="nav-button">About Us</button>
-      <button className="nav-button" style = {{backgroundColor: 'lightgreen'}} onClick={() => openOverlay('https://drive.google.com/file/d/1gQGCdgxe0Rz3BOt4cNnWT3pcK8zD8oZC/preview')}>Master Plan</button>
-      <button className="nav-button" style = {{backgroundColor: 'lightgreen'}} onClick={() => openOverlay('https://drive.google.com/file/d/1iqAGT2RX-BCe78QPZWo6xw1S7-jhJtHv/preview')}>Sector Maps</button>
+      <button
+        className="nav-button"
+        style={{ backgroundColor: 'lightgreen' }}
+        onClick={() =>
+          openOverlay(
+            'https://drive.google.com/file/d/1gQGCdgxe0Rz3BOt4cNnWT3pcK8zD8oZC/preview'
+          )
+        }
+      >
+        Master Plan
+      </button>
+      <div
+        className="nav-button"
+        style={{ position: 'relative', backgroundColor: 'lightgreen' }}
+        onMouseEnter={() => setShowSectorButtons(true)}
+        onMouseLeave={() => setShowSectorButtons(false)}
+      >
+        Sector Maps
+        {showSectorButtons && (
+          <div className="dropdown-menu">
+            <button
+              className="dropdown-button"
+              onClick={() =>
+                openOverlay(
+                  'https://drive.google.com/file/d/1OlcmKKFRXfNYOypdW_CklC--TTtZSmDS/preview'
+                )
+              }
+            >
+             <center> Sector 2</center>
+            </button>
+            <button
+              className="dropdown-button"
+              onClick={() =>
+                openOverlay(
+                  'https://drive.google.com/file/d/1SElfIPAjZ7rW-ES1E-DbvOS6xRuBgvkL/preview'
+                )
+              }
+            >
+             <center>  Sector 3 and 4</center> 
+            </button>
+            <button
+              className="dropdown-button"
+              onClick={() =>
+                openOverlay(
+                  'https://drive.google.com/file/d/1LLKNZYYPRVrUBNv2IjbqblgGI_YRdmWl/preview'
+                )
+              }
+            >
+             <center>  Sector 5</center> 
+            </button>
+          </div>
+        )}
+      </div>
       <button className="nav-button">Investment Options</button>
-      <button className="nav-button">Location</button>
+      <a target = "_blank" href = "https://maps.app.goo.gl/gzd2AApojANt438Q7">
+      <button
+        className="nav-button"
+        style={{ backgroundColor: 'lightgreen' }}
+      >Location</button></a>
       <button className="nav-button">Contact Us</button>
 
       {/* Overlay */}
@@ -29,9 +85,9 @@ const TopNavbar = () => {
         <div className="overlay">
           <div className="overlay-content">
             <button className="close-button" onClick={closeOverlay}>
-                
+              Close
             </button>
-            <iframe src={pdfUrl} width="100%" height="100%" allow="autoplay" title='maps'></iframe>
+            <iframe src={pdfUrl} width="100%" height="100%" allow="autoplay" title="maps"></iframe>
           </div>
         </div>
       )}
@@ -40,3 +96,5 @@ const TopNavbar = () => {
 };
 
 export default TopNavbar;
+
+
